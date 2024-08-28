bc3156fa67a370a6bea1819a1e7aa94ee0374976
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2024-08-28 14:09:57 +0600

    1st task done

---
+++ script.js
@@ -0,0 +1,6 @@
+var user_entry = prompt("Enter the temperature in Celcius")
+
+fahrenheit_convert = (9/5)*user_entry + 32
+
+alert("Fahrenheit: " + fahrenheit_convert + " Degree")
+console.log("Task Completed!!!");
